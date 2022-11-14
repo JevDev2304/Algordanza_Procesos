d79944766592d10059a1b64b78a8bbce4d368b61
Psychohistorian <cristianfrancoraigosa@gmail.com>
2022-11-13 22:28:52 -0500

    Cargando commit Algordanza

--- modelo.py
+++ modelo.py
@@ -10,7 +10,7 @@ Original file is located at
 from datetime import datetime
 import pandas as pd
 from openpyxl import Workbook
-#
+#"#
 class Diamante:
     TAMANOS = {0.2: "0.2", 0.3: "0,3", 0.4: "0,4", 0.5: "0,5", 0.6: "0,6", 0.7: "0,7", 0.8: "0,8", 0.9: "0,9", 1: "1"}
     ORIGEN = ["Mascota", "Cabello", "Cenizas"]
